7971d1e945d5b4ad2787a8d5953841cb8d8abb21
SamVonderahe <samvonderahe@gmail.com>
2020-01-25 12:24:27 -0500

    Update MakeSound.java

--- MakeSound.java
+++ MakeSound.java
@@ -16,7 +16,7 @@ public class MakeSound {
     private AudioFormat audioFormat;
     private SourceDataLine sourceLine;
 
-    /** 
+    /**
      * @param filename the name of the file that is going to be played
      */
     public void playSound(String filename){
@@ -32,13 +32,46 @@ public class MakeSound {
 
         try {
             audioStream = AudioSystem.getAudioInputStream(soundFile);
-        } catch (Exception e){ 
+        } catch (Exception e){
             e.printStackTrace();
             System.exit(1);
         }
 
         audioFormat = audioStream.getFormat();
 
-@                                                                               
-:set nonumber                                                 17,33         Top
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+        try {
+            sourceLine = (SourceDataLine) AudioSystem.getLine(info);
+            sourceLine.open(audioFormat);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            System.exit(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
+        sourceLine.start();
+
+        int nBytesRead = 0;
+        byte[] abData = new byte[BUFFER_SIZE];
+        while (nBytesRead != -1) {
+            try {
+                nBytesRead = audioStream.read(abData, 0, abData.length);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (nBytesRead >= 0) {
+                @SuppressWarnings("unused")
+                int nBytesWritten = sourceLine.write(abData, 0, nBytesRead);
+            }
+        }
+
+        sourceLine.drain();
+        sourceLine.close();
+    }
+    public static void main(String args[]) {
+      MakeSound beautiful_noise = new MakeSound();
+      beautiful_noise.playSound("noise/");
+    }
+}
